fc7bb250f69e24745b4f84cd922d2a7957cc8967
moonway <59399356+moonway-dev@users.noreply.github.com>
2024-06-13 16:53:03 +0300

    vercel

--- api/index.js
+++ api/index.js
@@ -87,3 +87,5 @@ app.post('/v3/marketplace/insert', authenticate, async (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
