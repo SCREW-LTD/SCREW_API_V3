f148483c00e2e6c904b8ff0b5f158c80a82b6082
moonway <59399356+moonway-dev@users.noreply.github.com>
2024-06-30 22:38:03 +0300

    cors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
-
+app.use(cors());
 app.use(express.json());
 
 const supabase = createClient(process.env.API_URL, process.env.API_KEY);
